2881279b32fffb6c4c41d6e7a2c5a68f75df623b
Luong Tuan Duong <ltduong1406@gmail.com>
2021-05-02 17:07:00 +0700

    2/5

--- app/src/main/java/com/example/appnote/MainActivity.java
+++ app/src/main/java/com/example/appnote/MainActivity.java
@@ -30,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //hl
 
         recyclerView = findViewById(R.id.recycler_view);
         fab = findViewById(R.id.fabAdd);
